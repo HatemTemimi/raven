d81869d3f14e48bdb38c5f94a14ea05db7fa9f5b
Hatem Temimi <hatem.altemimi@gmail.com>
2023-10-10 15:16:12 +0100

    fix: docker file

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,11 @@
-# Specifies a parent image
 FROM golang:1.20.5-bullseye
  
-WORKDIR /app
-COPY go.mod ./
-RUN go mod download
-RUN go get github.com/HatemTemimi/Raven/raven
-COPY *.go ./
-RUN go build -o /raven
-CMD [ “/raven” ]
+WORKDIR /
+
+COPY . .
+
+RUN go get
+
+RUN go build .
+
+ENTRYPOINT [ "/Raven" ] 
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+
 	"github.com/HatemTemimi/Raven/raven"
 )
 
@@ -10,36 +11,34 @@ func main() {
 	raven := raven.Raven{}
 	raven.Init()
 
-  target := flag.String("t", "", "Provide a Target URL to test proxies against, defaults to google")
-  fetch := flag.String("fetch", "all", "Fetch type; 1/all all proxies 2/valid only valid proxies[you must provide a target with -t]")
-  output := flag.String("o", "", "Path to the file, defaults to proxies.json")
-  help := flag.String("h", "", "Raven help")
-  flag.Parse()
+	target := flag.String("t", "", "Provide a Target URL to test proxies against, defaults to google")
+	fetch := flag.String("fetch", "all", "Fetch type; 1/all all proxies 2/valid only valid proxies[you must provide a target with -t]")
+	output := flag.String("o", "", "Path to the file, defaults to proxies.json")
+	help := flag.String("h", "", "Raven help")
+	flag.Parse()
 
-  if *fetch == "all" {
-    if *target != ""{
-      fmt.Println("the -t (target flag) is used with fetch set to valid --> -fetch valid")
-    } else if *target == "" && *output =="" {
-        raven.FetchAllToStdOut()
-    } else if *target == "" && *output !="" {
-        raven.FetchAllToJsonFile(*output) 
-    }
-  } else if *fetch == "valid" {
-    if *target == "" && *output=="" {
-      fmt.Println("no custom target provided, testing against default target.")
-      raven.FetchValidToStdOut("www.google.com")
-    } else if *target != "" && *output=="" {
-      raven.FetchValidToStdOut(*target)
-    } else if *target == "" && *output!="" {
-      fmt.Println("no custom target provided, testing against default target.")
-      raven.FetchValidToJsonFile("www.google.com", *output)
-    } else if *target != "" && *output!="" {
-      raven.FetchValidToJsonFile(*target, *output)
-    }
-  }
-
-  if *help != "" {
-      flag.PrintDefaults()
-  }
+	if *fetch == "all" {
+		if *target != "" {
+			fmt.Println("the -t (target flag) is used with fetch set to valid --> -fetch valid")
+		} else if *target == "" && *output == "" {
+			raven.FetchAllToStdOut()
+		} else if *target == "" && *output != "" {
+			raven.FetchAllToJsonFile(*output)
+		}
+	} else if *fetch == "valid" {
+		if *target == "" && *output == "" {
+			fmt.Println("no custom target provided, testing against default target.")
+			raven.FetchValidToStdOut("www.google.com")
+		} else if *target != "" && *output == "" {
+			raven.FetchValidToStdOut(*target)
+		} else if *target == "" && *output != "" {
+			fmt.Println("no custom target provided, testing against default target.")
+			raven.FetchValidToJsonFile("www.google.com", *output)
+		} else if *target != "" && *output != "" {
+			raven.FetchValidToJsonFile(*target, *output)
+		}
+	} else if *fetch == "" || *help != "" {
+		flag.PrintDefaults()
+	}
 
 }
