cc4858d68677f63fc2106cfce9c62d015d851955
Hatem Temimi <hatem.altemimi@gmail.com>
2023-10-12 14:29:59 +0100

    fix: cli refactor

--- main.go
+++ main.go
@@ -1,54 +1,66 @@
 package main
 
 import (
+	"flag"
 	"log"
-
 	"github.com/HatemTemimi/Raven/raven"
 )
 
 func main() {
 	raven := raven.Raven{}
 	raven.Init()
-	err := raven.FetchAllToJsonFile("proxies.json")
-	if err != nil {
-		log.Println(err)
-	}
-	err = raven.CheckFromJsonToJson("www.spankbang.com","proxies.json", "valid.json")
-	if err != nil {
-		log.Println(err)
-	}
 
-	/*
 	target := flag.String("t", "", "Provide a Target URL to test proxies against, defaults to google")
-	fetch := flag.String("f", "all", "Fetch type; 1/all all proxies 2/valid only valid proxies[you must provide a target with -t]")
-	output := flag.String("o", "", "Path to the file, defaults to proxies.json")
+	fetch := flag.String("f", "all", "Fetch types:\n 1/all: all proxies \n 2/valid: only valid proxies[you must provide a target with -t]")
+	output := flag.String("o", "", "Path to the output file, defaults to proxies.json")
+	//input := flag.String("i", "", "Path to the input file")
 	help := flag.String("h", "", "Raven help")
 
 	flag.Parse()
 
 	if *fetch == "all" {
+	//fetching all without validation
 		if *target != "" {
-			fmt.Println("the -t (target flag) is used with fetch set to valid --> -fetch valid")
-		} else if *target == "" && *output == "" {
-			raven.FetchAllToStdOut()
-		} else if *target == "" && *output != "" {
-			raven.FetchAllToJsonFile(*output)
+		//with target!!err 
+			log.Println("the -t (target flag) is used with fetch set to valid --> -fetch valid")
+		} else if *target == ""{
+			//without target
+			if *output == "" {
+				//without output
+				raven.FetchAllToStdOut()
+			} else if *output != "" {
+				//with output
+				raven.FetchAllToJsonFile(*output)
+			}
+
 		}
+
 	} else if *fetch == "valid" {
-		if *target == "" && *output == "" {
-			fmt.Println("no custom target provided, testing against default target.")
-			raven.FetchValidToStdOut("www.google.com")
-		} else if *target != "" && *output == "" {
-			raven.FetchValidToStdOut(*target)
-		} else if *target == "" && *output != "" {
-			fmt.Println("no custom target provided, testing against default target.")
-			raven.FetchValidToJsonFile("www.google.com", *output)
-		} else if *target != "" && *output != "" {
-			raven.FetchValidToJsonFile(*target, *output)
+		if *target == "" {
+		//fetching valid without target, forwards to google.com
+			if *output == ""{
+			//without output, forwards to sdtdout
+				log.Println("no custom target provided, testing against default target.")
+				raven.FetchValidToStdOut("www.google.com")
+			} else if *output != ""{
+			//with output
+				log.Println("no custom target provided, testing against default target.")
+				raven.FetchValidToJsonFile("www.google.com", *output)
+			}
+		} else if *target != "" {
+			//with target
+			if *output == ""{
+				//without output
+				raven.FetchValidToStdOut(*target)
+			} else if *output != "" {
+				//with output
+				raven.FetchValidToJsonFile(*target, *output)
+			}
 		}
-	} else if *fetch == "" || *help != "" {
+	}else if *fetch == "" || *help != "" {
+		//asking for help
 		flag.PrintDefaults()
 	}
-	*/
+	
 
 }
