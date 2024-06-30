5786fca1aafcec2f1a5a2e99e7aefc92cd1ef9ba
hatem.temimi <hatem.altemimi@gmail.com>
2024-06-30 11:58:38 +0100

    feat: proxies-size-adjust

--- ui-client/src/App.vue
+++ ui-client/src/App.vue
@@ -7,12 +7,17 @@ const target = ref('')
 const loading = ref(true)
 
 const fetchProxies = async () => {
+  loading.value = true
   await axios.get('http://localhost:8080/api/fetch')
       .then((response)=>{
-        proxies.value = response.data.slice(0, 31)
+        proxies.value = response.data.slice(0, 29)
         loading.value = false
-
       })
+}
+
+const refresh = ()=>{
+  proxies.value = []
+  fetchProxies()
 }
 
 const testProxies = async ()=>{
@@ -34,43 +39,49 @@ onMounted(async ()=>{
 
 <template>
   <div class="container" style="width: 100vw; height: 100vh; display: flex; justify-content: center ">
-    <div  style="width:80%; height: auto; display: flex; flex-direction: column; gap: 1vh">
-      <div style="height: 6%; display: flex; justify-content: center; align-items: center; margin-top: 1vh" class="bordered">
+    <div style="width:80%; height: auto; display: flex; flex-direction: column; gap: 1vh">
+      <div style="height: 6%; display: flex; justify-content: center; align-items: center; margin-top: 1vh"
+           class="bordered">
         Raven Proxy Lister & Tester
       </div>
-      <div  class="bordered">
-        <div style=" height: auto" class="bordered">
-          <div class="search" style="text-align: center; margin-top: 1vh">
-            <div style="display: flex; justify-content: center; ">
+      <div class="bordered">
+        <div style=" height: auto">
+          <div class="tools" style="margin-top: 2vh; display: flex; justify-content: space-between;">
+            <button @click="refresh" style="margin-left: 10px">Refresh</button>
+            <div class="search" style="width: 80%; display: flex; justify-content: center">
               <input v-model="target" style="width:25%" placeholder="type the target url you want to test against.."/>
-              <button @click="testProxies">test</button>
+              <button style="margin-left: 0.5rem" @click="testProxies">Test</button>
             </div>
+            <button style="margin-right: 10px">Copy to Clipboard</button>
           </div>
-          <div class="content" style="padding: 10px" >
-          <div style="margin-top: 2vh">
-            <div style="text-align: center" v-if="loading">loading..
-            </div>
-            <div style=" width:100%; display: flex; justify-content: space-between; flex-direction: row;" class="bordered">
-              <div style="margin-left: 5px">IP:Port</div>
-              <div>Status</div>
-              <div style="margin-right: 5px">Speed</div>
-            </div>
-            <div class="bordered" style="margin-top: 0.5vh; padding: 0.2rem">
-          <div v-for="proxy of proxies">
-            <div style="display: flex; justify-content: space-between; flex-direction: row;">
-              <div>
-                  {{ proxy.Ip }}: {{ proxy.Port}}
+          <div class="content" style="padding: 10px">
+            <div style="margin-top: 2vh; margin-bottom: 1vh">
+              <div style="text-align: center" v-if="loading">
+                hold on a second..
               </div>
-              <div>
-                {{ proxy.Status }}
+              <div v-if="!loading" style=" width:100%; display: flex; justify-content: space-between; flex-direction: row;"
+                   class="bordered">
+                <div style="margin-left: 5px">IP:Port</div>
+                <div>Status</div>
+                <div style="margin-right: 5px">Speed</div>
               </div>
-              <div>
-                {{ proxy.Speed  }}
+              <div class="bordered" style="margin-top: 0.5vh; padding: 0.2rem">
+                <div v-for="proxy of proxies">
+                  <div style="display: flex; justify-content: space-between; flex-direction: row;">
+                    <div>
+                      {{ proxy.Ip }}: {{ proxy.Port }}
+                    </div>
+                    <div>
+                      {{ proxy.Status }}
+                    </div>
+                    <div>
+                      {{ proxy.Speed }}
+                    </div>
+                  </div>
+                  <div class="line"></div>
+                </div>
               </div>
             </div>
-          </div>
-          </div>
-          </div>
           </div>
         </div>
       </div>
@@ -85,7 +96,10 @@ onMounted(async ()=>{
   will-change: filter;
   transition: filter 300ms;
 }
-
+.line {
+  flex: 1;
+  border-bottom: 1px dashed black;
+}
 .bordered {
   border-width: 2px;
   border-color: black;
